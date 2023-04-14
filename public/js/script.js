6d890615138c49f78c96d6fa559b689a3763aa38
Sajib Kumar Mitra <niloykesslar@gmail.com>
2023-04-14 13:12:27 +1000

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,60 +1,49 @@
-const getProjects = () => {
-    $.get('/api/projects',(response) => {
-        if(response.statusCode==200){
-            addCards(response.data);
-        }
-    })
-  }
-  
-  const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a boring day!")
-  }
-  
-  
-  const submitForm = () => {
-    let formData = {};
-    formData.title = $('#title').val();
-    formData.image = $('#image').val();
-    formData.link = $('#link').val();
-    formData.description = $('#description').val();
-   console.log("Form Data Submitted: ", formData);
-    addProjectToApp(formData);
-    
-  }
-    //ajax function...
-    const addProjectToApp = (project) => {
-        $.ajax({
-            url: '/api/projects',
-            data: project,
-            type: 'POST',
-            success: (result) => {
-                alert(result.message);
-                location.reload(); // it automatically reloads the page 
-            }
-        })
-    }
-    
-  const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
-    });
-  }
-  
-  
-  $(document).ready(function () {
-    $(".materialboxed").materialbox();
-    $("#formSubmit").click(() => {
-      submitForm();
-    });
-    getProjects();
-    addCards(cardList);
-    $(".modal").modal();
+const addCards = (items) => {
+  console.log(items);
+  items.forEach(item => {
+      let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">About this kiiten</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+      $('#card-section').append(itemToAppend);
   });
+}
+
+const submitForm = () => {
+  let formData = {};
+  formData.title = $('#title').val();
+  formData.image = $('#image').val();
+  formData.link = $('#link').val();
+  formData.description = $('#description').val();
+
+  console.log('form data: ', formData);
+  addCat(formData);
+}
+
+const getCats = () => {
+  $.get('/api/cats', (res) => {
+      if (res.statusCode === 200) {
+          addCards(res.data);
+      }
+  });
+}
+
+const addCat = (cat) => {
+  $.ajax({
+      url: 'api/cats',
+      data: cat,
+      type: 'POST',
+      success: (result) => {
+          alert(result.message);
+          location.reload();
+      }
+  });
+}
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+  $('.modal').modal();
+
+  getCats();
+
+  $('#formSubmit').click(()=>{
+      submitForm();
+  })
+});
