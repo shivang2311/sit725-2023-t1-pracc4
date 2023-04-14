a08cbf6b953542bf718d0e46ec1e7e8df0bc9be2
Sajib Kumar Mitra <niloykesslar@gmail.com>
2023-04-14 13:10:29 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,100 +1,57 @@
-var express = require("express")
-var app = express()
-var cors = require ('cors')
-let projectCollection;
+var express = require('express');
+var app = express();
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb+srv://sajibmitra3:admin@sajibmitra.osjuz0u.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+let dbCollection;
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
 
-//mongo db connection..
-const Mongoclient = require ('mongodb').MongoClient;
-
-//database conn
-const uri = 'mongodb+srv://sajibmitra3:admin@sajibmitra.osjuz0u.mongodb.net/?retryWrites=true&w=majority'
-const client = new Mongoclient (uri, {useNewUrlParser: true})
-
-
-//insert project
-const insertProjects = (project,callback) => {
-    projectCollection.insert(project,callback);
+function dbConnection(collectionName) {
+    client.connect(err => {
+        dbCollection = client.db().collection(collectionName);
+        if (!err) {
+            console.log('DB Connected');
+            console.log(dbCollection);
+        } else {
+            console.error(err);
+        }
+    });
 }
 
-const getProjects =(callback) =>{
-    projectCollection.find({}).toArray(callback);
-}
+app.post('/api/cats', (req, res) => {
+    let cat = req.body;
+    insert(cat, (err, result) => {
+        if (err) {
+            res.json({statusCode: 400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message: 'Cat Successfully Added'});
+        }
+    });
+});
 
-const createCollection = (collectionName) => {
-    client.connect((err,db) => {
-      projectCollection = client.db().collection(collectionName);
-       if (!err){
-      
-        console.log('MongoDb Connected')
-       }
-       else {
-        console.log("DB Error: ", err);
-        process.getMaxListeners(1);
-
-       }
-
-    
-        
-    } )
-}
-
-
-
-const cardList = [
-    {
-        title: "M Block",
-        image: "images/MGR.jpg",
-        link: "About M Block",
-        desciption: "Demo desciption about M Block"
-    },
-    {
-        title: "L Block",
-        image: "images/LBlock.jpg",
-        link: "About L Block",
-        desciption: "Demo desciption about L Block"
-    }
-]
-
-
-
-
-app.get('/api/projects',(req,res) => {
-    getProjects((err, result)=> {
-    if (err){
-        res.json({statusCode: 400,message: err})
-    }
-    else{
-        res.json({statusCode: 200, message:"success", data: result})
-    }
-    })
-
+app.get('/api/cats',(req,res) => {
+    getAllCats((err, result) => {
+        if (err) {
+            res.json({statusCode: 400, message: err});
+        } else {
+            res.json({statusCode: 200, data: result, message: 'Successful'});
+        }
+    });
 })
-//post api
-app.post('/api/projects',(req,res) => {
-    console.log("New Project added", req.body)
-    var newproject = req.body;
 
-    insertProjects(newproject,(err,result)=> {
-        if (err){
-            res.json({statusCode: 400,message: err})
-        }
-        else{
-            res.json({statusCode: 200, message:"project successfully added", data: result})
-        }
-        })
-    
-    })
+function insert(cat, callback) {
+    dbCollection.insertOne(cat, callback);
+}
 
+function getAllCats(callback){
+    dbCollection.find().toArray(callback);
+}
 
 var port = process.env.port || 3000;
-
-app.listen(port,()=>{
-    console.log("App listening to: http://localhost: "+port)
-    createCollection ("vit")
-}
-)
+app.listen(port, ()=> {
+    console.log('App listening to: ' + port);
+    dbConnection('Cats');
+})
